c5481da475a7e880b678248a1043edcdfd27a0ac
wimmer190074 <wimmer190074@sr.htlweiz.at>
2024-04-15 11:06:09 +0200

    fixed some bugs on the watched page

--- FilmDiary/src/components/FilmGallery.vue
+++ FilmDiary/src/components/FilmGallery.vue
@@ -1,65 +1,70 @@
 <template>
-  <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-4@m uk-padding" uk-grid>
-    <div v-for="film in films" :key="film.id">
-      <div class="uk-card uk-card-default card-container" @mouseover="hoverCard(true)" @mouseleave="hoverCard(false)">
-        <div class="uk-background-blend-overlay uk-background-secondary uk-background-cover uk-height-small uk-panel uk-flex uk-flex-center uk-flex-middle" :style="{ 'background-image': 'url(' + film.poster + ')' }" v-show="isHovered">
-          <div class="uk-card-body card-body">
-            <h3 class="col">{{ film.title }}</h3>
-            <p class="col">{{ film.description }}</p>
+    <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-4@m uk-padding" uk-grid>
+      <div v-for="film in films" :key="film.id">
+        <div class="uk-card uk-card-default card-container" @mouseover="hoverCard(film.id, true)" @mouseleave="hoverCard(film.id, false)">
+          <div class="uk-background-blend-overlay uk-background-secondary uk-background-cover uk-height-small uk-panel uk-flex uk-flex-center uk-flex-middle" :style="{ 'background-image': 'url(' + film.poster + ')' }" v-show="isHovered === film.id">
+            <div class="uk-card-body card-body">
+              <h3 class="col">{{ film.title }}</h3>
+              <p class="col">{{ film.description }}</p>
+            </div>
           </div>
-        </div>
-        <div class="uk-background-cover uk-height-small uk-panel uk-flex uk-flex-center uk-flex-middle" :style="{ 'background-image': 'url(' + film.poster + ')' }" v-if="!isHovered">
-          <div class="uk-card-body card-body">
-            <h3 class="col">{{ film.title }}</h3>
-            <p class="col">{{ film.description }}</p>
+          <div class="uk-background-cover uk-height-small uk-panel uk-flex uk-flex-center uk-flex-middle" :style="{ 'background-image': 'url(' + film.poster + ')' }" v-if="isHovered !== film.id">
+            <div class="uk-card-body card-body">
+              <h3 class="col">{{ film.title }}</h3>
+              <p class="col">{{ film.description }}</p>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-</template>
-
-<script lang="ts">
-export default {
-  props: {
-    films: {
-      type: Array,
-      default: () => []
+  </template>
+    
+  <script lang="ts">
+  export default {
+    props: {
+      films: {
+        type: Array,
+        default: () => []
+      }
+    },
+    data() {
+      return {
+        isHovered: {}
+      };
+    },
+    methods: {
+    hoverCard(id, status) {
+        if (status) {
+            this.isHovered = id;
+        } else {
+            this.isHovered = null; // or any value that indicates no film is hovered
+        }
     }
-  },
-  data() {
-    return {
-      isHovered: false
-    };
-  },
-  methods: {
-    hoverCard(status) {
-      this.isHovered = status;
-    }
+}
+  };
+  </script>
+  
+  <style>
+  .card-container {
+    height: 500px;
+    width: auto;
   }
-};
-</script>
-
-<style>
-.card-container {
-  height: 500px; /* Set the height of the card to be 100% of the viewport height */
-  width: auto;
-}
-
-.uk-background-cover {
-  background-size: cover; /* Make the background image cover the entire container */
-  height: 100%; /* Ensure the background image covers the entire card area */
-}   
-
-.col {
-  color: white;
-}
-
-.card-body {
-  display: none; /* Initially hide the card body */
-}
-
-.card-container:hover .card-body {
-  display: block; /* Display the card body when hovering over the card container */
-}
-</style>
+  
+  .uk-background-cover {
+    background-size: cover;
+    height: 100%;
+  }   
+  
+  .col {
+    color: white;
+  }
+  
+  .card-body {
+    display: none;
+  }
+  
+  .card-container:hover .card-body {
+    display: block;
+  }
+  </style>
+  
