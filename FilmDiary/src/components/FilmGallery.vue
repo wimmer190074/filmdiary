3ccfcc0be3da3546a1f4f2c4548dbe99384a4942
wimmer190074 <wimmer190074@sr.htlweiz.at>
2024-04-15 10:37:47 +0200

    finished Gallery Design

--- FilmDiary/src/components/FilmGallery.vue
+++ FilmDiary/src/components/FilmGallery.vue
@@ -1,30 +1,65 @@
 <template>
-    <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-4@m padtop" uk-grid>
-      <div v-for="film in films" :key="film.id">
-        <div class="uk-card uk-card-default">
-          <div class="uk-card-media-top">
-            <img :src="film.poster" width="1800" height="1000" alt="">
+  <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-4@m uk-padding" uk-grid>
+    <div v-for="film in films" :key="film.id">
+      <div class="uk-card uk-card-default card-container" @mouseover="hoverCard(true)" @mouseleave="hoverCard(false)">
+        <div class="uk-background-blend-overlay uk-background-secondary uk-background-cover uk-height-small uk-panel uk-flex uk-flex-center uk-flex-middle" :style="{ 'background-image': 'url(' + film.poster + ')' }" v-show="isHovered">
+          <div class="uk-card-body card-body">
+            <h3 class="col">{{ film.title }}</h3>
+            <p class="col">{{ film.description }}</p>
           </div>
-          <div class="uk-card-body">
-            <h3 class="uk-card-title">{{ film.title }}</h3>
-            <p>{{ film.description }}</p>
+        </div>
+        <div class="uk-background-cover uk-height-small uk-panel uk-flex uk-flex-center uk-flex-middle" :style="{ 'background-image': 'url(' + film.poster + ')' }" v-if="!isHovered">
+          <div class="uk-card-body card-body">
+            <h3 class="col">{{ film.title }}</h3>
+            <p class="col">{{ film.description }}</p>
           </div>
         </div>
       </div>
     </div>
-  </template>
-  
-  <script lang="ts">
-  export default {
-    props: {
-      films: {
-        type: Array,
-        default: () => []
-      }
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    films: {
+      type: Array,
+      default: () => []
     }
-  };
-  </script>
-  
-  <style>
-  </style>
-  
+  },
+  data() {
+    return {
+      isHovered: false
+    };
+  },
+  methods: {
+    hoverCard(status) {
+      this.isHovered = status;
+    }
+  }
+};
+</script>
+
+<style>
+.card-container {
+  height: 500px; /* Set the height of the card to be 100% of the viewport height */
+  width: auto;
+}
+
+.uk-background-cover {
+  background-size: cover; /* Make the background image cover the entire container */
+  height: 100%; /* Ensure the background image covers the entire card area */
+}   
+
+.col {
+  color: white;
+}
+
+.card-body {
+  display: none; /* Initially hide the card body */
+}
+
+.card-container:hover .card-body {
+  display: block; /* Display the card body when hovering over the card container */
+}
+</style>
